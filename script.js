75ea37197916e6eb52c37aee3eeaf395fd5a275f
YokoyamaRyo8 <yokoyama.ryo8@gmail.com>
2026-02-08 01:11:06 +0900

    add javascript basics

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Hello JavaScript");
+let name = "横山";
+let age = 15;
+
+console.log(name);
+console.log(age);
+
+function greet(name) {
+  console.log("こんにちは " + name);
+}
+
+greet("横山");
+
+
+console.log(age);
+
+let checkAge = 16;
+
+if (checkAge >= 18) {
+  console.log("大人です");
+} else {
+  console.log("未成年です");
+}
+const title = document.getElementById("title");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function () {
+  title.textContent = "押されました！";
+});
